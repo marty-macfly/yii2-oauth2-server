8351ec505c79f2cb5b4c0e372404a07cfc317fb9
Charles DELFLY <c.delfly@linkbynet.com>
2017-03-29 15:09:05 +0200

    Add fix blameable behavior

--- src/models/OauthAccessTokens.php
+++ src/models/OauthAccessTokens.php
@@ -5,7 +5,7 @@ namespace macfly\oauth2server\models;
 use Yii;
 use yii\behaviors\BlameableBehavior;
 
-class OauthAccessTokens extends filsh\yii2\oauth2server\models\OauthAccessTokens
+class OauthAccessTokens extends \filsh\yii2\oauth2server\models\OauthAccessTokens
 {
 	/**
 	 * @inheritdoc
@@ -13,10 +13,22 @@ class OauthAccessTokens extends filsh\yii2\oauth2server\models\OauthAccessTokens
 	public function behaviors()
 	{
 		return [
-			[
+			'BlameableBehavior'	=> [
 				'class' => BlameableBehavior::className(),
 				'createdByAttribute' => 'user_id',
 				'updatedByAttribute' => false,
+				'value'	=> function ($event)
+					{
+						$behavior						= $this->getBehavior('BlameableBehavior');
+						$createdByAttribute	= $behavior->createdByAttribute;
+
+						if($event->name == 'beforeInsert' && !isset($this->$createdByAttribute))
+						{
+							return $this->$createdByAttribute;
+						}
+
+						return $behavior->value;
+					},
 			],
 		];
 	}
