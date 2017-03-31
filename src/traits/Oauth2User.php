1bff352850634a7963ebaa325e8cabe0d2b1b73a
Charles DELFLY <c.delfly@linkbynet.com>
2017-03-31 12:20:53 +0200

    Use token id provide instead of the user logged in

--- src/traits/Oauth2User.php
+++ src/traits/Oauth2User.php
@@ -12,7 +12,9 @@ trait Oauth2User
   public static function findIdentityByAccessToken($token, $type = null)
   {
     /** @var \filsh\yii2\oauth2server\Module $module */
-    $module = Yii::$app->getModule('oauth2');
+    $module																					= Yii::$app->getModule('oauth2');
+		$module->getRequest()->headers['AUTHORIZATION'] = sprintf("Bearer %s", $token);
+		$module->getServer()->getResourceController()->verifyResourceRequest($module->getRequest(), new \OAuth2\Response());
     $token = $module->getServer()->getResourceController()->getToken();
     return !empty($token['user_id'])
                 ? static::findIdentity($token['user_id'])
