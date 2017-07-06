7b8ebdf23185bcd8b852070b729b678cb3524f68
Charles DELFLY <c.delfly@linkbynet.com>
2017-07-06 13:23:27 +0200

    Switch to kartik-v/yii2-widget-datetimepicker

--- src/views/admin/accesstokens/_form.php
+++ src/views/admin/accesstokens/_form.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
-use yii\jui\DatePicker;
+use kartik\datecontrol\DateControl;
 
 use filsh\yii2\oauth2server\models\OauthClients;
 
@@ -14,33 +14,35 @@ use filsh\yii2\oauth2server\models\OauthClients;
 
 <div class="oauth-access-tokens-form">
 
-  <?php $form = ActiveForm::begin(); ?>
+	<?php $form = ActiveForm::begin(); ?>
 
-  <?= $form->field($model, 'access_token')->textInput(['maxlength' => true]) ?>
+	<?= $form->field($model, 'access_token')->textInput(['maxlength' => true]) ?>
 
-  <?= $form->field($model, 'client_id', [
-	  'inputOptions' => [
-    'autofocus' => 'autofocus',
-    'class' => 'form-control',
-    'tabindex' => '1']])->dropDownList(ArrayHelper::map(OauthClients::find()->all(), 'client_id', 'client_id'));
-  ?>
-
-	<?= $form->field($model, 'expires')->widget(DatePicker::classname(), [
-		'dateFormat' => 'yyyy-MM-dd',
-	]);?>
-
-	<?php
-    if(\Yii::$app->user->can($module->adminRole))
-    {
-			echo $form->field($model, 'user_id')->textInput();
-    }
+	<?= $form->field($model, 'client_id', [
+		'inputOptions' => [
+		'autofocus' => 'autofocus',
+		'class' => 'form-control',
+		'tabindex' => '1']])->dropDownList(ArrayHelper::map(OauthClients::find()->all(), 'client_id', 'client_id'));
 	?>
 
-  <?= $form->field($model, 'scope')->textInput(['maxlength' => true]) ?>
+	<?= $form->field($model, 'expires')->widget(DateControl::classname(), [
+		'type'					=> 'datetime',
+		'displayFormat'	=> 'php:Y-m-d H:i:s',
+		]);
+	?>
 
-  <div class="form-group">
- 		<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-  </div>
+	<?php
+		if(\Yii::$app->user->can($module->adminRole))
+		{
+			echo $form->field($model, 'user_id')->textInput();
+		}
+	?>
+
+	<?= $form->field($model, 'scope')->textInput(['maxlength' => true]) ?>
+
+	<div class="form-group">
+		<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+	</div>
 
 	<?php ActiveForm::end(); ?>
 
