214317e0bbe4d903063b4776644ef780e7dd326a
Charles DELFLY <c.delfly@linkbynet.com>
2017-11-15 19:03:44 +0700

    Add getAuthKey() method

--- src/traits/Oauth2User.php
+++ src/traits/Oauth2User.php
@@ -4,6 +4,9 @@ namespace macfly\oauth2server\traits;
 
 use Yii;
 
+use macfly\oauth2server\models\OauthAccessTokens;
+use macfly\oauth2server\models\OauthClients;
+
 trait Oauth2User
 {
     /**
@@ -12,7 +15,7 @@ trait Oauth2User
     public static function findIdentityByAccessToken($token, $type = null)
     {
         /** @var \filsh\yii2\oauth2server\Module $module */
-        $module																					= Yii::$app->getModule('oauth2');
+        $module                                                                                    = Yii::$app->getModule('oauth2');
         $module->getRequest()->headers['AUTHORIZATION'] = sprintf("Bearer %s", $token);
         $module->getServer()->getResourceController()->verifyResourceRequest($module->getRequest(), new \OAuth2\Response());
         $token = $module->getServer()->getResourceController()->getToken();
@@ -40,5 +43,32 @@ trait Oauth2User
     {
         $user = static::findByUsername($username);
         return ['user_id' => $user->getId()];
+    }
+
+    /**
+    * Implemented for Oauth2 Interface
+    */
+    public function getAuthKey()
+    {
+        if (($client = OauthClients::findOne(['user_id' => $this->id])) === null) {
+            $client                = new OauthClients();
+            $client->client_id     = $this->username;
+            $client->user_id       = $this->id;
+            $client->client_secret = substr(hash('sha512', mt_rand() . mt_rand() . mt_rand() . mt_rand() . microtime(true) . uniqid(mt_rand(), true)), 0, 32);
+            $client->redirect_uri  = '.';
+            $client->grant_types   = 'client_credentials';
+            $client->save();
+        }
+
+        $token               = new OauthAccessTokens();
+        $token->client_id    = $client->client_id;
+        $token->user_id      = $client->user_id;
+        $token->expires      = Yii::$app->formatter->asDatetime(time() + 3600, 'yyyy-MM-dd HH:mm:ss');
+        $token->access_token = substr(hash('sha512', mt_rand() . mt_rand() . mt_rand() . mt_rand() . microtime(true) . uniqid(mt_rand(), true)), 0, 40);
+        if ($token->save()) {
+            return $token->access_token;
+        }
+
+        return null;
     }
 }
